2cfedd3d5beafb7e4b7d87bb3b2ab4fb9bbb8ad0
Emiliano Gonzalez <emigonzalez3333@gmail.com>
2023-09-17 19:33:34 -0300

    mensaje de inicio agregado

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { tiposFormulario } from "./logica/generarFormulario.js";
 import { procesarFormulario } from "./logica/procesarPreguntas.js";
 
 const formulario = document.getElementById("form");
-
+/*
 for (const preguntaNumero in cuestionario) {
   let tipo = cuestionario[preguntaNumero].tipo;
   const divGenerar = tiposFormulario[tipo](cuestionario[preguntaNumero]);
@@ -15,3 +15,4 @@ for (const preguntaNumero in cuestionario) {
 }
 
 procesarFormulario(formulario);
+*/
